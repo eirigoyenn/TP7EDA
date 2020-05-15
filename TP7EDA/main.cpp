cdb893fdfda6116b5ddb78b1a960437505bf53b9
Unknown <ndeleon@itba.edu.ar>
2020-05-14 23:37:08 -0300

    Ejemplo

---
+++ TP7EDA/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "lodepng.h"
+
+int main(void) {
+
+	unsigned char* img;
+	unsigned int w, h;
+
+	lodepng_decode32_file(&img, &w, &h, "img.png");
+	for (int i = 0; i < w * h; i++) {
+		unsigned int c = 4 * i + 2;
+		img[c] = 255;
+	}
+
+	lodepng_encode32_file("nuevaimagen.png", img, w, h);
+	free(img);
+
+	return 0;
+}
