0fd2032854a56ca3fce264304f51e316fa093e02
Unknown <ndeleon@itba.edu.ar>
2020-05-15 02:04:09 -0300

    Add File .EDA

--- TP7EDA/main.cpp
+++ TP7EDA/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Compresor.h"
+#include <fstream>
 
 int main(void) {
 
@@ -7,8 +8,14 @@ int main(void) {
 
 	c.decodeFile("img.png");
 
-	c.compress(c.img, c.w, c.h, "");
 
+	c.compress(c.img, c.w, c.h, to_string(c.h*c.w));
+
+	fstream fs;
+	fs.open("test.EDA");
+
+	fs << c.testing;
+	fs.close();
 	cout << c.testing << endl;
 	return 0;
 }
